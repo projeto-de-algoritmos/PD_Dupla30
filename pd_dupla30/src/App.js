fe80e4133b35b9d290edc517d00280b163eb673f
joaoCeilandia <joaovitordmrosa@hotmail.com>
2022-04-18 13:19:32 -0300

    visual improvments

--- pd_dupla30/src/App.js
+++ pd_dupla30/src/App.js
@@ -1,66 +1,110 @@
 import React, { useState, useEffect } from 'react'
-import { Gantt, Task, EventOption, StylingOption, ViewMode, DisplayOption } from 'gantt-task-react';
+import { Gantt } from 'gantt-task-react';
+import { Button, Input } from 'antd';
 import "gantt-task-react/dist/index.css";
-import './App.css';
+
+const initialTaskData = {
+  name: 'nova tarefa',
+  weight: 1,
+  start: new Date(2022, 4, 8),
+  end: new Date(2022, 4, 9) 
+}
+
+const tasksInputMap = [
+  { key: 'name', label: 'Nome'},
+  { key: 'weight', label: 'peso'},
+  { key: 'start', label: 'dia de inicio'},
+  { key: 'end', label: 'dia de fim'}
+]
+
+const initialTasksDAta = [ 
+  { 
+    name: 'tarefa 1 - 3', 
+    weight: 3, 
+    start: new Date(2022, 4, 8), 
+    end: new Date(2022, 4, 9) 
+  }, 
+  { 
+    name: 'tarefa 2 - 2', 
+    weight: 2, 
+    start: new Date(2022, 4, 9), 
+    end: new Date(2022, 4, 10) 
+  }, 
+  { 
+    name: 'tarefa 3 - 5', 
+    weight: 5, 
+    start: new Date(2022, 4, 12), 
+    end: new Date(2022, 4, 16) 
+  } 
+]
 
 function App() {
-  var auxMap = { 0: 0 }; 
+  const [auxMap, setAuxMap] = useState({}); 
+  const [newTaskData,setNewTaskData] = useState(initialTaskData);
   const [lastItems, setLastItems] = useState({}); 
-  const [tasks,setTasks] = useState([ 
-    { 
-      name: 'teste 1', 
-      weight: 3, 
-      start: new Date(2022, 4, 9), 
-      end: new Date(2022, 4, 10) 
-    }, 
-    { 
-      name: 'teste 2', 
-      weight: 2, 
-      start: new Date(2022, 4, 8), 
-      end: new Date(2022, 4, 9) 
-    }, 
-    { 
-      name: 'teste 3', 
-      weight: 5, 
-      start: new Date(2022, 4, 12), 
-      end: new Date(2022, 4, 16) 
-    } 
-  ]); 
+  const [totalResult, setTotalResult] = useState(null); 
+  const [resultTasks,setResultTasks] = useState([]);
+  const [sortedTasks,setSortedTasks] = useState(initialTasksDAta);
+  const [tasks,setTasks] = useState(initialTasksDAta); 
  
-  const solve = index => { 
- 
-    if(index === -1 || index === undefined) { 
+  const solve = index => {
+    if(index === -1) { 
       return 0; 
     } 
      
     if(auxMap[index]) { 
       return auxMap[index]; 
     } 
-    console.log(tasks[index]?.weight, solve(lastItems[index]), solve(index-1)) 
     const res = Math.max(tasks[index]?.weight + solve(lastItems[index]), solve(index-1)); 
-    auxMap = { ...auxMap, [index]: res }; 
+    setAuxMap(prev => ({ ...prev, [index]: res })); 
     return res; 
   } 
  
   const getLastItem = index => { 
     for (var i = index; i >= 0; i--) { 
-      if(tasks[i].end < tasks[index].start) return i; 
+      if(tasks[i].end <= tasks[index].start) return i; 
    } 
    return -1; 
   } 
  
-  useEffect(() => { 
+  useEffect(() => {
     tasks.forEach((item, index) => { 
       setLastItems(prev => ({ ...prev, [index]: getLastItem(index)})); 
-    }) 
-  },[]) 
- 
+    });
+    setSortedTasks(tasks.sort((a,b) => {
+      if (a.end > b.end) {
+        return 1;
+      }
+      if (a.end < b.end) {
+        return -1;
+      }
+      return 0;
+    }));
+  }, [tasks]);
+
   return ( 
     <> 
-    {JSON.stringify(lastItems)} 
-    {solve(tasks.length-1)} 
-    {JSON.stringify(auxMap)} 
-    <Gantt tasks={tasks} />
+    <div>
+      {tasksInputMap.map(task => (
+        <div>
+          <label>{task.label}:</label>
+          <Input onChange={e => setNewTaskData(prev => ({ ...prev, [task.key]: e.target.value}))}/>
+        </div>
+      ))}
+    <Button type="primary" onClick={() => {
+      const _newTaskData = { name:`${newTaskData.name} - ${newTaskData.weight}` , weight: Number(newTaskData.weight), start: new Date(2022, 4, newTaskData.start), end: new Date(2022, 4, newTaskData.end)}
+      setTasks(prev => ([...prev, _newTaskData]));
+    }}>Adicionar</Button>
+    </div>
+    <Gantt tasks={sortedTasks} />
+    <Button type="primary"  onClick={() => {
+      setTotalResult(solve(tasks.length-1));
+
+    }}> Calcular</Button>
+    <label>{totalResult}</label>
+    {resultTasks.length ? (
+      <Gantt tasks={resultTasks} />
+    ) : null}
     </> 
   ); 
 }
