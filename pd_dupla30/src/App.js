69df8659eebff7c2f5464b4681b1d482d62d51ea
Nícalo Ribeiro <nicalo63@gmail.com>
2022-04-18 11:26:17 -0300

    Funcoes base com mock
    Co-authored-by: Joao Vitor <joaovitordmrosa@hotmail.com>

--- pd_dupla30/src/App.js
+++ pd_dupla30/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import { Gantt, Task, EventOption, StylingOption, ViewMode, DisplayOption } from 'gantt-task-react';
+import "gantt-task-react/dist/index.css";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  var auxMap = { 0: 0 }; 
+  const [lastItems, setLastItems] = useState({}); 
+  const [tasks,setTasks] = useState([ 
+    { 
+      name: 'teste 1', 
+      weight: 3, 
+      start: new Date(2022, 4, 9), 
+      end: new Date(2022, 4, 10) 
+    }, 
+    { 
+      name: 'teste 2', 
+      weight: 2, 
+      start: new Date(2022, 4, 8), 
+      end: new Date(2022, 4, 9) 
+    }, 
+    { 
+      name: 'teste 3', 
+      weight: 5, 
+      start: new Date(2022, 4, 12), 
+      end: new Date(2022, 4, 16) 
+    } 
+  ]); 
+ 
+  const solve = index => { 
+ 
+    if(index === -1 || index === undefined) { 
+      return 0; 
+    } 
+     
+    if(auxMap[index]) { 
+      return auxMap[index]; 
+    } 
+    console.log(tasks[index]?.weight, solve(lastItems[index]), solve(index-1)) 
+    const res = Math.max(tasks[index]?.weight + solve(lastItems[index]), solve(index-1)); 
+    auxMap = { ...auxMap, [index]: res }; 
+    return res; 
+  } 
+ 
+  const getLastItem = index => { 
+    for (var i = index; i >= 0; i--) { 
+      if(tasks[i].end < tasks[index].start) return i; 
+   } 
+   return -1; 
+  } 
+ 
+  useEffect(() => { 
+    tasks.forEach((item, index) => { 
+      setLastItems(prev => ({ ...prev, [index]: getLastItem(index)})); 
+    }) 
+  },[]) 
+ 
+  return ( 
+    <> 
+    {JSON.stringify(lastItems)} 
+    {solve(tasks.length-1)} 
+    {JSON.stringify(auxMap)} 
+    <Gantt tasks={tasks} />
+    </> 
+  ); 
 }
 
 export default App;
